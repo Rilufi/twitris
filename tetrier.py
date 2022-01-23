306efe461d355398ff25fedc6afd20b543eea644
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-22 23:10:38 -0300

    Update tetrier.py

--- tetrier.py
+++ tetrier.py
@@ -1,20 +1,19 @@
 from auth import api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato
 import random
 
-toReply = "rilufix"
 
 commands = ['⬅️', '➡️', '⤴️', '⬇️']
+bots = [api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato]
 
-bots = ['api_xame', 'api_uva', 'api_mevu', 'api_cor', 'api_naso', 'api_gato']
 
 def tetris(api):
-	tweets = api.user_timeline(screen_name = toReply, count=1)
+	tweets = api.user_timeline(screen_name = "rilufix", count=1)
 	chosen = random.choice(commands)
 
 	mystring = f""" {chosen}"""
 	
 	for tweet in tweets:
-        	api.update_status("@" + toReply + mystring, in_reply_to_status_id = tweet.id)
+        	api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
 
 for bot in bots:
     tetris(bot)
