478feca9105c833405aabfc0a56d6791bee3a837
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-24 12:53:37 -0300

    Update tetrier.py

--- tetrier.py
+++ tetrier.py
@@ -6,15 +6,19 @@ commands = ['⬅️', '➡️', '⤴️', '⬇️']
 bots = [api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato]
 
 def tetris(api):
-	tweets = api.user_timeline(screen_name="rilufix", count=1, exclude_replies = True)
+	tweets = api.user_timeline(screen_name="rilufix", count=1, exclude_replies = True,  tweet_mode = 'extended')
 	chosen = random.choice(commands)
 	mystring = f""" {chosen}"""
 	
-	for tweet in tweets:
-		api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
+	for info in tweets:
+		if "ArXiv" not in info.full_text:
+			for tweet in tweets:
+				api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
+		else:
+			sys.exit()
 
 for bot in bots:
-	try:	
+	try:
 		tetris(bot)
 	except:
 		pass
