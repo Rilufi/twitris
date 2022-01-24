b9cc63d838d817f6bc986629a44744bc608899a6
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-23 23:34:34 -0300

    Update tetrier.py

--- tetrier.py
+++ tetrier.py
@@ -4,15 +4,13 @@ import sys
 
 commands = ['⬅️', '➡️', '⤴️', '⬇️']
 bots = [api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato]
-blocks= ["⬛", "⬜", "🟦", "🟥", "🟨", "🟧","🟪","🟩","🟫"]
 
 def tetris(api):
 	tweets = api.user_timeline(screen_name = "rilufix", count=1)
 	chosen = random.choice(commands)
 	mystring = f""" {chosen}"""
-	res = [ele for ele in blocks if(ele in tweets)]
-
-	if bool(res) != False:
+	
+	if "⬛" or "⬜":
 		for tweet in tweets:
 	        	api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
 	else:
