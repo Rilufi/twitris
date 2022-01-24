dc476c35d7a8d4e14df5d607f1be16710459d37b
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-01-24 14:32:33 -0300

    Update tetrier.py

--- tetrier.py
+++ tetrier.py
@@ -1,27 +1,25 @@
-from auth import api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato
+from auth import api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato, api_ril
 import random
 import sys
 
 commands = ['⬅️', '➡️', '⤴️', '⬇️']
 bots = [api_xame, api_uva, api_mevu, api_cor, api_naso, api_gato]
+tweets = api_ril.user_timeline(screen_name="rilufix", count=1, exclude_replies = True,  tweet_mode = 'extended')
 
 def tetris(api):
-	tweets = api.user_timeline(screen_name="rilufix", count=1, exclude_replies = True,  tweet_mode = 'extended')
 	chosen = random.choice(commands)
 	mystring = f""" {chosen}"""
-	
-	for info in tweets:
-		if "⬜" in info.full_text:
-			for tweet in tweets:
-				api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
-		elif "⬛" in info.full_text:
-			for tweet in tweets:
-				api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
-		else:
-			sys.exit()
-			
+	for tweet in tweets:
+		api.update_status("@" + "rilufix" + mystring, in_reply_to_status_id = tweet.id)
+
 for bot in bots:
 	try:
-		tetris(bot)
+		for info in tweets:
+			if "⬜" in info.full_text:
+				tetris(bot)
+			elif "⬛" in info.full_text:
+				tetris(bot)
+			else:
+				sys.exit()
 	except:
 		pass
