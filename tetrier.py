cbe18811fb7cf9159eefe9bcc9c3b37a926902eb
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-23 08:47:00 -0300

    Update tetrier.py

--- tetrier.py
+++ tetrier.py
@@ -7,24 +7,18 @@ bots = [api_naso, api_gato]
 blocks = ["🟦", "🟥", "🟨", "🟧", "🟪", "🟩", "🟫"]
 tweets = api_cor.user_timeline(screen_name="botoronga", count=1, exclude_replies = True,  tweet_mode = 'extended')
 
-def tetris(api):
+def tetris():
 	chosen = random.choice(commands)
 	mystring = f""" {chosen}"""
 	for tweet in tweets:
-		api.update_status("@" + "botoronga" + mystring, in_reply_to_status_id = tweet.id)
+		api_naso.update_status("@" + "botoronga" + mystring, in_reply_to_status_id = tweet.id)
+                api_gato.update_status("@" + "botoronga" + mystring, in_reply_to_status_id = tweet.id)
 
 for info in tweets:
-	if any(block in info.full_text for block in blocks):
-		for bot in bots:
-			try:	
-				tetris(bot)
-			except:
-				pass
-	#elif "⬜" or "⬛" in info.full_text:
-	#	for bot in bots:
-	#		try:	
-	#			tetris(bot)
-	#		except:
-	#			pass
+	if any(block in info.full_text for block in blocks:
+	    try:	
+	        tetris()
+	    except:
+	        passs
 	else:
-		sys.exit()
+	    sys.exit()
