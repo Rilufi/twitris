7bee9325d0510bfec686edcc181bb2876c6fec60
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-11-14 16:32:51 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -4,10 +4,10 @@ import os
 
 #Rilufix
 
-consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
-consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
-access_token = os.environ["TWITTER_ACCESS_TOKEN_KEY"]
-access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN_KEY")
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,10 +16,10 @@ api = tweepy.API(auth)
 
 #Botoronga
 
-consumer_key = os.environ["CONSUMER_KEY_COR"]
-consumer_secret = os.environ["CONSUMER_SECRET_COR"]
-access_token = os.environ["ACCESS_TOKEN_COR"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_COR"]
+consumer_key = os.environ.get("CONSUMER_KEY_COR")
+consumer_secret = os.environ.get("CONSUMER_SECRET_COR")
+access_token = os.environ.get("ACCESS_TOKEN_COR")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_COR")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -28,10 +28,10 @@ api_cor = tweepy.API(auth)
 
 #Gato
 
-consumer_key = os.environ["CONSUMER_KEY_GATO"]
-consumer_secret = os.environ["CONSUMER_SECRET_GATO"]
-access_token = os.environ["ACCESS_TOKEN_GATO"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_GATO"]
+consumer_key = os.environ.get("CONSUMER_KEY_GATO")
+consumer_secret = os.environ.get("CONSUMER_SECRET_GATO")
+access_token = os.environ.get("ACCESS_TOKEN_GATO")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_GATO")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -40,10 +40,10 @@ api_gato = tweepy.API(auth)
 
 #Nasa
 
-consumer_key = os.environ["CONSUMER_KEY_NASO"]
-consumer_secret = os.environ["CONSUMER_SECRET_NASO"]
-access_token = os.environ["ACCESS_TOKEN_NASO"]
-access_token_secret = os.environ["ACCESS_TOKEN_SECRET_NASO"]
+consumer_key = os.environ.get("CONSUMER_KEY_NASO")
+consumer_secret = os.environ.get("CONSUMER_SECRET_NASO")
+access_token = os.environ.get("ACCESS_TOKEN_NASO")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET_NASO")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
